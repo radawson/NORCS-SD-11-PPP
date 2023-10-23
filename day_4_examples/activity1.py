72f50d4604d18a705f8b51116ffb9a9ba31c1ce6
radawson <dawsora@gmail.com>
2023-10-23 19:22:50 -0400

    activity 1

--- day_4_examples/activity1.py
+++ day_4_examples/activity1.py
@@ -3,5 +3,28 @@ unsorted_list =  [('English', 88), ('Science', 90), ('Maths', 97), ('Social scie
 sorted_list = sorted(unsorted_list,key=lambda x:x[1])
 
 #unsorted_list.sort(key=lambda x:x[1])
-print(unsorted_list)
-print(sorted_list)
+
+original_list = [3, 6, 9, 2]
+cubed_list = list(map(lambda x: x**3, original_list))
+cubed_list2 = [x**3 for x in original_list]
+
+is_even = lambda x: x % 2 ==0
+even_list = [is_even(x) for x in original_list]
+
+list1 = [i for i in range(1,101)]
+ 
+sentence = "The quick brown fox jumped over the fence."
+word_lengths = {word: len(word) for word in sentence.split()}
+
+if __name__ == "__main__":
+    print(unsorted_list)
+    print(sorted_list)
+
+    print(cubed_list)
+    print(cubed_list2)
+
+    print(even_list)
+
+    print(list1)
+
+    print(word_lengths)
