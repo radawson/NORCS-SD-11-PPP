3e4b4ca2f24fcd80b1a29584e0b6cd9d79d50e11
radawson <dawsora@gmail.com>
2023-10-23 18:57:59 -0400

    activity 4-1

--- day_4_examples/activity1.py
+++ day_4_examples/activity1.py
@@ -1,1 +1,7 @@
+unsorted_list =  [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
 
+sorted_list = sorted(unsorted_list,key=lambda x:x[1])
+
+#unsorted_list.sort(key=lambda x:x[1])
+print(unsorted_list)
+print(sorted_list)
